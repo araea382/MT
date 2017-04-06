116e7705b1c9bfa032d8329d63eddb4d9a75a111
Araya Eamrurksiri <araea382@student.liu.se>
2017-04-06 16:59:11 +0200

    no state

--- Code/results.R
+++ Code/results.R
@@ -1,4 +1,8 @@
 library(MSwM2)
+library(ecp)
+library(ggplot2)
+library(reshape2)
+
 # one test case per SW
 predictor <- c("RrcConnectionSetupComplete","Paging","X2HandoverRequest","DuProdName","Fdd.Tdd","NumCells")
 fmla <- as.formula(paste("TotCpu ~ ", paste(predictor, collapse= "+")))
@@ -43,12 +47,25 @@ plotReg(mswm_L16B_3, expl=predictor[3], regime=3)
 # newdata <- test_g2_L16B[1,]
 # predict(mswm_L16B_3, newdata)
 
+#--------------------------------#
 # change the swiching parameter
 switch <- rep(TRUE,length(mod_L16B$coefficients)+1+1)
 switch[c(5,6)] <- FALSE; switch
 set.seed(1)
 mswm_L16B_3 <- MSwM2::msmFit(mod_L16B, k=3, p=1, sw=switch, control=list(trace=TRUE, maxiter=1000, parallel=FALSE))
 summary(mswm_L16B_3)
+
+#--------------------------------#
+# smoothed prob plot
+L16B_3 <- as.data.frame(mswm_L16B_3@Fit@smoProb)
+L16B_3 <- cbind(index=seq(1,nrow(L16B_3)),L16B_3)
+colnames(L16B_3) <- c("index","regime1","regime2","regime3")
+
+L16B_3 <- melt(L16B_3, id="index")
+ggplot(data=L16B_3, aes(x=index, y=value, colour=variable)) + geom_line() +
+  ylab("Smoothed Probabilities") + ggtigle("L16B") +
+  theme_bw() + theme(legend.title = element_blank())
+
 
 ###############
 # g2 L16B
@@ -78,6 +95,7 @@ plotReg(mswm_L16B_2, expl=predictor[5], regime=2)
 plotReg(mswm_L16B_2, expl=predictor[6], regime=1)
 plotReg(mswm_L16B_2, expl=predictor[6], regime=2)
 
+#--------------------------------#
 # change the swiching parameter
 switch <- rep(TRUE,length(mod_L16B$coefficients)+1+1)
 switch[c(3,4,5,6,11)] <- FALSE; switch
@@ -85,51 +103,63 @@ set.seed(1)
 mswm_L16B_2 <- MSwM2::msmFit(mod_L16B, k=2, p=1, sw=switch, control=list(trace=TRUE, maxiter=500, parallel=FALSE))
 summary(mswm_L16B_2)
 
+#--------------------------------#
+# smoothed prob plot
+L16B_2 <- as.data.frame(mswm_L16B_2@Fit@smoProb)
+L16B_2 <- cbind(index=seq(1,nrow(L16B_2)),L16B_2)
+colnames(L16B_2) <- c("index","regime1","regime2")
+
+L16B_2 <- melt(L16B_2, id="index")
+ggplot(data=L16B_2, aes(x=index, y=value, colour=variable)) + geom_line() +
+  ylab("Smoothed Probabilities") + ggtitle("L16B") +
+  theme_bw() + theme(legend.title = element_blank())
+
+
 ###############
 # g2 L16B
 # 4 states
 ###############
-set.seed(1)
-mswm_L16B_4 <- MSwM2::msmFit(mod_L16B, k=4, p=1, sw=rep(TRUE,length(mod_L16B$coefficients)+1+1), control=list(trace=TRUE, maxiter=500, parallel=FALSE))
-summary(mswm_L16B_4)
-# 1433.622 1818.238 -672.811
+# set.seed(1)
+# mswm_L16B_4 <- MSwM2::msmFit(mod_L16B, k=4, p=1, sw=rep(TRUE,length(mod_L16B$coefficients)+1+1), control=list(trace=TRUE, maxiter=500, parallel=FALSE))
+# summary(mswm_L16B_4)
+# # 1433.622 1818.238 -672.811
+#
+# plot(mswm_L16B_4)
+#
+# plotDiag(mswm_L16B_4, which=1)
+# plotDiag(mswm_L16B_4, which=2)
+# plotDiag(mswm_L16B_4, which=3)
+#
+# plotProb(mswm_L16B_4, which=1)
+# plotProb(mswm_L16B_4, which=2)
+# plotProb(mswm_L16B_4, which=3)
+#
+# plotReg(mswm_L16B_4, expl=predictor[4], regime=1)
+# plotReg(mswm_L16B_4, expl=predictor[4], regime=2)
+#
+# plotReg(mswm_L16B_4, expl=predictor[5], regime=1)
+# plotReg(mswm_L16B_4, expl=predictor[5], regime=2)
+#
+# plotReg(mswm_L16B_4, expl=predictor[6], regime=1)
+# plotReg(mswm_L16B_4, expl=predictor[6], regime=2)
+#
+# #--------------------------------#
+# # change the swiching parameter
+# switch <- rep(TRUE,length(mod_L16B$coefficients)+1+1)
+# switch[c(3,4,5,6,11)] <- FALSE; switch
+# set.seed(1)
+# mswm_L16B_4 <- MSwM2::msmFit(mod_L16B, k=4, p=1, sw=switch, control=list(trace=TRUE, maxiter=500, parallel=FALSE))
+# summary(mswm_L16B_4)
 
-plot(mswm_L16B_4)
-
-plotDiag(mswm_L16B_4, which=1)
-plotDiag(mswm_L16B_4, which=2)
-plotDiag(mswm_L16B_4, which=3)
-
-plotProb(mswm_L16B_4, which=1)
-plotProb(mswm_L16B_4, which=2)
-plotProb(mswm_L16B_4, which=3)
-
-plotReg(mswm_L16B_4, expl=predictor[4], regime=1)
-plotReg(mswm_L16B_4, expl=predictor[4], regime=2)
-
-plotReg(mswm_L16B_4, expl=predictor[5], regime=1)
-plotReg(mswm_L16B_4, expl=predictor[5], regime=2)
-
-plotReg(mswm_L16B_4, expl=predictor[6], regime=1)
-plotReg(mswm_L16B_4, expl=predictor[6], regime=2)
-
-# change the swiching parameter
-switch <- rep(TRUE,length(mod_L16B$coefficients)+1+1)
-switch[c(3,4,5,6,11)] <- FALSE; switch
-set.seed(1)
-mswm_L16B_4 <- MSwM2::msmFit(mod_L16B, k=4, p=1, sw=switch, control=list(trace=TRUE, maxiter=500, parallel=FALSE))
-summary(mswm_L16B_4)
 
 ###############
 # g2 L16A
 # 3 states
 ###############
-# predictor <- c("RrcConnectionSetupComplete","Paging","X2HandoverRequest","DuProdName","Fdd.Tdd","NumCells")
-predictor <- c("RrcConnectionSetupComplete","Paging","X2HandoverRequest","Fdd.Tdd","NumCells")
-# predictor <- c("RrcConnectionSetupComplete","Paging","X2HandoverRequest","DuProdName","Fdd.Tdd")
-fmla <- as.formula(paste("TotCpu ~ ", paste(predictor, collapse= "+")))
+predictor2 <- c("RrcConnectionSetupComplete","Paging","X2HandoverRequest","Fdd.Tdd","NumCells")
+fmla2 <- as.formula(paste("TotCpu ~ ", paste(predictor2, collapse= "+")))
 
-mod_L16A <- lm(fmla, data=train_g2_L16A)
+mod_L16A <- lm(fmla2, data=train_g2_L16A)
 summary(mod_L16A)
 alias(mod_L16A)
 
@@ -161,6 +191,17 @@ plotReg(mswm_L16A_3, expl=predictor[3], regime=1)
 plotReg(mswm_L16A_3, expl=predictor[3], regime=2)
 plotReg(mswm_L16A_3, expl=predictor[3], regime=3)
 
+#--------------------------------#
+# smoothed prob plot
+L16A_3 <- as.data.frame(mswm_L16A_3@Fit@smoProb)
+L16A_3 <- cbind(index=seq(1,nrow(L16A_3)),L16A_3)
+colnames(L16A_3) <- c("index","regime1","regime2","regime3")
+
+L16A_3 <- melt(L16A_3, id="index")
+ggplot(data=L16A_3, aes(x=index, y=value, colour=variable)) + geom_line() +
+  ylab("Smoothed Probabilities") + ggtitle("L16A") +
+  theme_bw() + theme(legend.title = element_blank())
+
 
 ###############
 # g2 L16A
@@ -189,6 +230,17 @@ plotReg(mswm_L16A_2, expl=predictor[2], regime=2)
 
 plotReg(mswm_L16A_2, expl=predictor[3], regime=1)
 plotReg(mswm_L16A_2, expl=predictor[3], regime=2)
+
+#--------------------------------#
+# smoothed prob plot
+L16A_2 <- as.data.frame(mswm_L16A_2@Fit@smoProb)
+L16A_2 <- cbind(index=seq(1,nrow(L16A_2)),L16A_2)
+colnames(L16A_2) <- c("index","regime1","regime2")
+
+L16A_2 <- melt(L16A_2, id="index")
+ggplot(data=L16A_2, aes(x=index, y=value, colour=variable)) + geom_line() +
+  ylab("Smoothed Probabilities") + ggtitle("L16A") +
+  theme_bw() + theme(legend.title = element_blank())
 
 
 ###############
@@ -226,6 +278,17 @@ plotReg(mswm_L17A_3, expl=predictor[3], regime=1)
 plotReg(mswm_L17A_3, expl=predictor[3], regime=2)
 plotReg(mswm_L17A_3, expl=predictor[3], regime=3)
 
+#--------------------------------#
+# smoothed prob plot
+L17A_3 <- as.data.frame(mswm_L17A_3@Fit@smoProb)
+L17A_3 <- cbind(index=seq(1,nrow(L17A_3)),L17A_3)
+colnames(L17A_3) <- c("index","regime1","regime2","regime3")
+
+L17A_3 <- melt(L17A_3, id="index")
+ggplot(data=L17A_3, aes(x=index, y=value, colour=variable)) + geom_line() +
+  ylab("Smoothed Probabilities") + ggtitle("L17A") +
+  theme_bw() + theme(legend.title = element_blank())
+
 
 ###############
 # g2 L17A
@@ -254,4 +317,65 @@ plotReg(mswm_L17A_2, expl=predictor[2], regime=2)
 
 plotReg(mswm_L17A_2, expl=predictor[3], regime=1)
 plotReg(mswm_L17A_2, expl=predictor[3], regime=2)
+
+
+#--------------------------------#
+# smoothed prob plot
+L17A_2 <- as.data.frame(mswm_L17A_2@Fit@smoProb)
+L17A_2 <- cbind(index=seq(1,nrow(L17A_2)),L17A_2)
+colnames(L17A_2) <- c("index","regime1","regime2")
+
+L17A_2 <- melt(L17A_2, id="index")
+ggplot(data=L17A_2, aes(x=index, y=value, colour=variable)) + geom_line() +
+  ylab("Smoothed Probabilities") + ggtitle("L17A") +
+  theme_bw() + theme(legend.title = element_blank())
+
+
+
+###############
+# ecp
+# g2 L16B
+###############
+library(ecp)
+Ediv_L16B <- e.divisive(matrix(train_g2_L16B$TotCpu), R=499, alpha=1)
+Ediv_L16B$k.hat # 2 clusters
+Ediv_L16B$estimates # 153
+out <- Ediv_L16B$estimates[c(-1,-length(Ediv_L16B$estimates))]
+
+dat <- data.frame(index=seq(1,nrow(train_g2_L16B)), TotCpu=train_g2_L16B$TotCpu)
+ggplot(data=dat, aes(x=index, y=TotCpu)) + geom_line() +
+  geom_vline(xintercept=out, colour="red", linetype="dashed") +
+  ggtitle("E-divisive L16B") + theme_bw()
+
+
+###############
+# ecp
+# g2 L16A
+###############
+Ediv_L16A <- e.divisive(matrix(train_g2_L16A$TotCpu), R=499, alpha=1)
+Ediv_L16A$k.hat # 1 cluster
+Ediv_L16A$estimates
+out <- Ediv_L16A$estimates[c(-1,-length(Ediv_L16A$estimates))]
+
+dat <- data.frame(index=seq(1,nrow(train_g2_L16A)), TotCpu=train_g2_L16A$TotCpu)
+ggplot(data=dat, aes(x=index, y=TotCpu)) + geom_line() +
+  geom_vline(xintercept=out, colour="red", linetype="dashed") +
+  ggtitle("E-divisive L16A") + theme_bw()
+# cannot detect anything
+
+
+###############
+# ecp
+# g2 L17A
+###############
+Ediv_L17A <- e.divisive(matrix(train_g2_L17A$TotCpu), R=499, alpha=1)
+Ediv_L17A$k.hat # 2 clusters
+Ediv_L17A$estimates # 75
+out <- Ediv_L17A$estimates[c(-1,-length(Ediv_L17A$estimates))]
+
+dat <- data.frame(index=seq(1,nrow(train_g2_L17A)), TotCpu=train_g2_L17A$TotCpu)
+ggplot(data=dat, aes(x=index, y=TotCpu)) + geom_line() +
+  geom_vline(xintercept=out, colour="red", linetype="dashed") +
+  ggtitle("E-divisive L17A") + theme_bw()
+
 
--- Code/results_plot.R
+++ Code/results_plot.R
@@ -1,6 +1,6 @@
 r <- as.data.frame(mswm_L16B_3@Fit@smoProb)
 r <- cbind(index=seq(1,nrow(r)),r)
-
+colnames(r) <- c("index","regime1","regime2","regime3")
 
 plot(r1, type="l", col="red", ylim=c(0,1), ylab="Smoothed Probabilities", xlim=c(0,250), xlab="")
 points(r2, type="l", col="green")
@@ -12,11 +12,13 @@ ggplot(data=r, aes(index)) + geom_line(aes(y=V1, colour="V1")) + geom_line(aes(y
 
 library(reshape2)
 r <- melt(r, id="index")
-ggplot(data=r, aes(x=index, y=value, colour=variable)) + geom_line() + theme_bw()
+ggplot(data=r, aes(x=index, y=value, colour=variable)) + geom_line() +
+  ylab("Smoothed Probabilities") + theme_bw() + theme(legend.title = element_blank())
 
 
 
 r2 <- as.data.frame(mswm_L16B_2@Fit@smoProb)
 r2 <- cbind(index=seq(1,nrow(r2)),r2)
 r2 <- melt(r2, id="index")
-ggplot(data=r2, aes(x=index, y=value, colour=variable)) + geom_line() + theme_bw()
+ggplot(data=r2, aes(x=index, y=value, colour=variable)) + geom_line() +
+  ylab("Smoothed Probabilities") + theme_bw()
