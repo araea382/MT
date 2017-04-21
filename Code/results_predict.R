0b96ed28a4f3b689f88941022eeeebaf7ade51f2
Araya Eamrurksiri <araea382@student.liu.se>
2017-04-21 08:13:36 +0200

    predict plot

--- Code/results_predict.R
+++ Code/results_predict.R
@@ -15,6 +15,8 @@ pred_L16A <- MSwM2::statePredict(mswm_L16A_NN, test_g2_L16A) ##FIX
 # [64,] 1.00000000  0.000000e+00  0.000000e+00
 
 
+
+
 #----------------------------------------------------------------#
 
 pred_L16B <- MSwM2::statePredict(mswm_L16B_NYY, test_g2_L16B)
@@ -22,6 +24,17 @@ pred_L16B <- MSwM2::statePredict(mswm_L16B_NYY, test_g2_L16B)
 # 1  1  1  1  1  3  1  3  3  3  1  3  3  3  2  3  3  3  3  3  3  3  1  3  1
 
 # plot with state area
+gen <- function(object,data){
+    state <- sapply(1:nrow(data), function(x) which.max(object@Fit@smoProb[x,]))
+    state <- factor(state)
+    index=seq(1,nrow(data))
+    xmin=index-0.5
+    xmax=index+0.5
+    y=data$TotCpu
+    ans <- data.frame(index,xmin,xmax,state,y=y,ymin=min(y),ymax=max(y))
+    return(ans)
+}
+
 gen2 <- function(x,dat,data){
     state <- factor(x)
     index=seq(1,nrow(data))
@@ -35,7 +48,7 @@ gen2 <- function(x,dat,data){
     return(ans)
 }
 
-
+state_L16B_3 <- gen(mswm_L16B_NYY, train_g2_L16B)
 state_test_L16B <- gen2(pred_L16B, state_L16B_3, test_g2_L16B)
 ggplot(data=state_test_L16B, aes(x=index, y=y)) + geom_line() +
     geom_rect(data=state_test_L16B, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, fill=state), alpha=0.2, inherit.aes=FALSE) +
@@ -49,6 +62,7 @@ pred_L17A <- MSwM2::statePredict(mswm_L17A_NNN, test_g2_L17A)
 # 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 
 # 1  1  1  2  1  3  2  3  3  2  2  2  2  2  2 
 
+state_L17A_3 <- gen(mswm_L17A_NNN, train_g2_L17A)
 state_test_L17A <- gen2(pred_L17A, state_L17A_3, test_g2_L17A)
 ggplot(data=state_test_L17A, aes(x=index, y=y)) + geom_line() +
   geom_rect(data=state_test_L17A, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, fill=state), alpha=0.2, inherit.aes=FALSE) +
