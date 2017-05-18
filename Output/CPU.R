b9701f4ac293a16c6b8d03c6d22632c00b7a878b
Araya Eamrurksiri <araea382@student.liu.se>
2017-05-18 17:02:01 +0200

    from ytd

--- Output/CPU.R
+++ Output/CPU.R
@@ -20,9 +20,7 @@ g2_L17A2 <- get_subset(g2_extract2, "L17A") #144 -> 102
 #-----------------------------------------------------#
 # fit linear model
 # DuProdName and Fdd/Tdd only has one factor
-predictor <- c("RrcConnectionSetupComplete","NumCells")
-fmla <- as.formula(paste("TotCpu ~ ", paste(predictor, collapse= "+")))
-mod_L16B <- lm(fmla, data=g2_L16B2)
+mod_L16B <- lm(TotCpu~1, data=g2_L16B2)
 summary(mod_L16B)
 
 # perform Markov switching autoregressive model
@@ -47,30 +45,37 @@ plotArea(mswm_L16B_2)
 
 
 gen <- function(object,data){
-  state <- apply(object@Fit@smoProb,1,which.max)
-  # state <- sapply(1:nrow(data), function(x) which.max(object@Fit@smoProb[x,]))
-  state <- factor(state)
-  index=seq(1,nrow(data))
-  xmin=index-0.5
-  xmax=index+0.5
-  y=data$TotCpu
-  ans <- data.frame(index,xmin,xmax,state,y=y,ymin=50,ymax=300)
-  return(ans)
+    state <- apply(object@Fit@smoProb,1,which.max)
+    # state <- sapply(1:nrow(data), function(x) which.max(object@Fit@smoProb[x,]))
+    state <- factor(state)
+    index=seq(1,nrow(data))
+    xmin=index-0.5
+    xmax=index+0.5
+    y=data$TotCpu
+    ans <- data.frame(index,xmin,xmax,state,y=y,ymin=50,ymax=300)
+    return(ans)
 }
 
 state_L16B_3 <- gen(mswm_L16B2, g2_L16B2)
 ggplot(data=state_L16B_3, aes(x=index, y=y)) + geom_line() +
-  geom_rect(data=state_L16B_3, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, fill=state), alpha=0.2, inherit.aes=FALSE) +
-  scale_fill_manual(values=c("red","green","blue")) + 
-  ylab("TotCpu") + theme_bw() + 
-  theme(legend.position="bottom") + coord_cartesian(ylim = c(50, 300)) 
+    geom_rect(data=state_L16B_3, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, fill=state), alpha=0.2, inherit.aes=FALSE) +
+    scale_fill_manual(values=c("red","green","blue")) + 
+    ylab("TotCpu") + theme_bw() + 
+    theme(legend.position="bottom") + coord_cartesian(ylim = c(50, 300)) 
 
 set.seed(1)
 Ediv_L16B <- e.divisive(matrix(g2_L16B2$TotCpu), R=499, min.size=5)
 Ediv_L16B$estimates
 out_L16B <- Ediv_L16B$estimates[c(-1,-length(Ediv_L16B$estimates))]
+# 10  64 136
 
 dat <- data.frame(index=seq(1,nrow(g2_L16B2)), TotCpu=g2_L16B2$TotCpu)
 ggplot(data=dat, aes(x=index, y=TotCpu)) + geom_line() + scale_color_manual(values=c("#F8766D","#00BA38","#619CFF")) +
-  geom_vline(xintercept=out_L16B, colour="red", linetype="longdash") +
-  ggtitle("E-divisive L16B") + theme_bw() + coord_cartesian(ylim = c(50, 300)) 
+    geom_vline(xintercept=out_L16B, colour="red", linetype="longdash") +
+    ggtitle("E-divisive L16B") + theme_bw() + coord_cartesian(ylim = c(50, 300)) 
+
+
+pred_state_L16B <- sapply(1:nrow(g2_L16B2), function(x) which.max(mswm_L16B_3@Fit@smoProb[x,]))
+chg_mswm_L16B <- which(diff(pred_state_L16B) != 0) + 1
+# 3   4  10  64 137
+
